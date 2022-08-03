1a34b61fe39438f15a0d284ddfa0ea37d8a93561
villageworks <villageworks@gmail.com>
2022-08-03 22:06:43 +0900

    初期画面（全商品表示／商品操作切り替え）の実装

--- src/main/java/la/servlet/ItemServlet.java
+++ src/main/java/la/servlet/ItemServlet.java
@@ -1,7 +1,6 @@
 package la.servlet;
 
 import java.io.IOException;
-import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,10 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import la.bean.ItemBean;
-import la.dao.DAOException;
-import la.dao.ItemDAO;
 
 /**
  * Servlet implementation class ItemServlet
@@ -35,18 +30,8 @@ public class ItemServlet extends HttpServlet {
 		String nextPage = "pages/errInternal.html";
 		// actionキーによって処理を分岐
 		if (action == null || action.isEmpty()) {
-			try {
-				// actionキーが未送信または未入力の場合：全商品一覧の表示
-				ItemDAO dao = new ItemDAO();
-				List<ItemBean> list = dao.findAll();
-				// 取得した商品リストをリクエストスコープに登録
-				request.setAttribute("items", list);
-				// 遷移先URLを設定
-				nextPage = "pages/showAllItem.jsp";
-			} catch (DAOException e) {
-				// TODO 自動生成された catch ブロック
-				e.printStackTrace();
-			}
+			// 遷移先URLを設定
+			nextPage = "pages/top.jsp";
 		}
 		// 画面遷移
 		RequestDispatcher dispatcher = request.getRequestDispatcher(nextPage);
---
+++ src/main/webapp/pages/top.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="UTF-8">
+		<title>Show All Item</title>
+	</head>
+	<body>
+		<p><a href="ItemServlet?action=all">全商品表示</a></p>
+		<p><a href="ItemServlet?action=operation">商品操作</a></p>
+	</body>
+</html>
